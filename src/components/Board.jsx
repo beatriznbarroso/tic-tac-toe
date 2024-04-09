567c9b504629ccf104673bd909a962e8950cec47
Beatriz Barroso <beatriznb@goparity.com>
2024-04-09 21:19:54 +0100

    react tic tac toe

---
+++ src/components/Board.jsx
@@ -0,0 +1,21 @@
+import Tile from "./Tile";
+import Strike from "./Strike";
+
+function Board({tiles, onTileClick, playerTurn, strikeClass}) {
+  return (
+    <div className="board">
+      <Tile value={tiles[0]} playerTurn={playerTurn} onClick={() => onTileClick(0)} className="right-border bottom-border"/>
+      <Tile value={tiles[1]} playerTurn={playerTurn} onClick={() => onTileClick(1)}className="right-border bottom-border"/>
+      <Tile value={tiles[2]} playerTurn={playerTurn} onClick={() => onTileClick(2)}className="bottom-border"/>
+      <Tile value={tiles[3]} playerTurn={playerTurn} onClick={() => onTileClick(3)}className="right-border bottom-border"/>
+      <Tile value={tiles[4]} playerTurn={playerTurn} onClick={() => onTileClick(4)}className="right-border bottom-border"/>
+      <Tile value={tiles[5]} playerTurn={playerTurn} onClick={() => onTileClick(5)}className="bottom-border"/>
+      <Tile value={tiles[6]} playerTurn={playerTurn} onClick={() => onTileClick(6)}className="right-border"/>
+      <Tile value={tiles[7]} playerTurn={playerTurn} onClick={() => onTileClick(7)}className="right-border"/>
+      <Tile value={tiles[8]} playerTurn={playerTurn} onClick={() => onTileClick(8)}/>
+      <Strike strikeClass={strikeClass} />
+    </div>
+  );
+}
+
+export default Board;
